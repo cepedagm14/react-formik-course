c75988733e61a153a9c6205324a15acf65e89468
cepedagm14 <cepedagm14@gmail.com>
2022-05-19 00:16:02 -0400

    manejo de estados y submit usando formik

--- src/components/YouTubeForm.jsx
+++ src/components/YouTubeForm.jsx
@@ -1,25 +1,50 @@
 import React from "react";
 import { useFormik } from "formik";
 
-
 const YouTubeForm = () => {
   const formik = useFormik({
-
-    })
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+    onSubmit: (values) => {
+      console.log("values", values);
+    },
+  });
   return (
     <div>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
 
         <label htmlFor="email">email</label>
-        <input type="text" id="email" name="email" />
+        <input
+          type="text"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
 
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
+      <pre>{JSON.stringify(formik.values, null, 2)}</pre>
     </div>
   );
 };
