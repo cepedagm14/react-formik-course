55e818cf353bb89eb61389049bd53df2628efa2f
cepedagm14 <cepedagm14@gmail.com>
2022-05-21 00:10:27 -0400

    refactor form using Formik, Form, Field

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import YouTubeForm from './components/YouTubeForm'
 import "./App.css"
+import YouTubeFormFormik from './components/YouTubeFormFormik'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
      <YouTubeForm />
+
     </div>
   )
 }
