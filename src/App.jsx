bcf1176bfd6b1c8a4f1fafc807fd24eba114aca5
cepedagm14 <cepedagm14@gmail.com>
2022-05-25 22:04:47 -0400

    nested objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 import YouTubeForm from './components/YouTubeForm'
 import "./App.css"
-import YouTubeFormFormik from './components/YouTubeFormFormik'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
--- src/components/YouTubeForm.jsx
+++ src/components/YouTubeForm.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Formik, Field, ErrorMessage, Form } from "formik";
 import * as Yup from "yup";
+import TextError from "./TextError";
 
 const initialValues = {
   name: "",
   email: "",
   channel: "",
+  commenst: "",
+  address: "",
+  social: {
+    facebook: "",
+    twitter: "",
+  },
 };
 const onSubmit = (values) => {
   console.log("submit", values);
@@ -28,17 +35,48 @@ const YouTubeForm = () => {
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name" />
-          <ErrorMessage name="name" />
+          <ErrorMessage name="name" component={TextError} />
         </div>
         <div className="form-control">
           <label htmlFor="email">email</label>
           <Field type="text" id="email" name="email" />
-          <ErrorMessage name="email" />
+          <ErrorMessage name="email">
+            {(errorMsg) => <div className="error">{errorMsg}</div>}
+          </ErrorMessage>
         </div>
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
           <Field type="text" id="channel" name="channel" />
           <ErrorMessage name="channel" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="commenst">Commenst</label>
+          <Field as="textarea" id="commenst" name="commenst" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <Field id="address" name="address">
+            {(props) => {
+              const { field, form, meta } = props;
+              // console.log(props);
+              return (
+                <div>
+                  <input type="text" id="address" {...field}></input>
+                  {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                </div>
+              );
+            }}
+          </Field>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="facebook">facebook</label>
+          <Field type="text" id="facebook" name="social.facebook" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="twitter">twitter</label>
+          <Field type="text" id="twitter" name="social.twitter" />
         </div>
         <button type="submit">Submit</button>
       </Form>
